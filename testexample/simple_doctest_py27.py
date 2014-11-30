7a557d17edea0b5c528508215cc3c4cda0014289
ccat <deadlack@gmail.com>
2014-11-30 22:35:50 +0900

    unittest example added

---
+++ testexample/simple_doctest_py27.py
@@ -0,0 +1,22 @@
+﻿# encoding=utf-8
+#-------------------------------------------------------------------------------
+# Description:
+# Author:      ccat
+# Created:     30/Nov/2014
+#-------------------------------------------------------------------------------
+
+def doctestTarget(a=1,b=1):
+    """
+    >>> doctestTarget(2,2)
+    4
+    >>> doctestTarget(a=2,b=2.5)
+    5.0
+    """
+    return a*b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+
---
+++ testexample/simple_unittest_py27.py
@@ -0,0 +1,36 @@
+﻿# encoding=utf-8
+#-------------------------------------------------------------------------------
+# Description:
+# Author:      ccat
+# Created:     30/Nov/2014
+#-------------------------------------------------------------------------------
+
+
+import unittest
+
+def testTarget(a=1,b=1):
+    return a*b
+
+
+class UnittestExample(unittest.TestCase):
+    def setUp(self):
+        self.a=1
+        self.b=1
+
+    def test_a(self):
+        self.assertEqual(testTarget(self.a),1)
+        self.a=2
+        self.assertEqual(testTarget(self.a),2)
+
+    def test_b(self):
+        self.assertEqual(testTarget(self.a,self.b),1)
+        self.b=2
+        self.assertEqual(testTarget(b=self.b),2)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
